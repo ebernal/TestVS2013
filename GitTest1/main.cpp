9836b28480373d2afc07f8eb396ab7d47952af1d
Enrique Bernal <ebernalgil@hotmail.com>
2015-04-07 11:19:23 +0200

    Simple functionality

--- GitTest1/main.cpp
+++ GitTest1/main.cpp
@@ -2,7 +2,10 @@
 
 int main(int argc, char** argv)
 {
-    std::cout << "Hello world!\n";
+    int sum = 0;
+    for(int i=1; i<=10; i++) sum += i;
+
+    std::cout << "Sum of first ten integers is " << sum << std::endl;
 
     return 0;
 }
